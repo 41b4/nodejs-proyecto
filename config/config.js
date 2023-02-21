5b94b833a0b1a434faf93d6d77d561a3da8e29ce
Alba C <albacabadotorrado@gmail.com>
2023-02-21 23:54:57 +0100

    produccion

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports={
-    HOST: process.env.HOST,
-    USER: process.env.USER,
-    PASSWORD: process.env.PASSWORD,
-    DATABASE: process.env.DATABASE
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 }
